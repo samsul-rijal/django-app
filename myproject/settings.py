d8621969ae6594fdf2c956e6e12c18b61b71610e
Samsul Rijal <samsulr99@gmail.com>
2025-05-02 14:52:41 +0700

    modifikasi style user list and form

--- myproject/settings.py
+++ myproject/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         'NAME': 'db_blog',
         'USER': 'postgres',
         'PASSWORD': 'admin',
-        'HOST': 'db',  # BUKAN 'localhost', tapi 'db' sesuai nama service docker-compose
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
